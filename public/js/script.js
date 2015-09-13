cbfa6d9991b79d4ab61f84eb49a8ccd4d5238cfd
Zander Martineau <zander@martineau.tv>
2015-09-13 21:46:11 +0100

    initial commit:
    - pasting in a package.json generates a markdown output using depdoc

---
+++ public/js/script.js
@@ -0,0 +1,30 @@
+var ajax = require('atomicjs');
+
+document.addEventListener('DOMContentLoaded', function() {
+
+	var submitBtn = document.querySelector('.submit');
+	var resultContainer = document.querySelector('.depdocResult');
+
+	submitBtn.addEventListener('click', function(e) {
+		e.preventDefault();
+		var spinner = document.createElement('div');
+		spinner.className = 'spinner';
+
+		resultContainer.innerHTML = '';
+		resultContainer.appendChild(spinner);
+		resultContainer.classList.add('is-loading');
+
+		var input = document.querySelector('.packageInput').value;
+		var normalisedInput = input.replace(/\r?\n|\s|\r/g,'');
+
+		ajax.post('/depdoc', normalisedInput, 'application/json')
+			.success(function (data, xhr) {
+				resultContainer.classList.remove('is-loading');
+				resultContainer.innerText = xhr.response;
+			})
+			.error(function (data, xhr) {
+				resultContainer.innerHTML = 'Ooops.. there was a problem. We are working on it.';
+			});
+	});
+
+});
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var depdoc = require('depdoc');
+var fs = require('fs');
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	res.render('index', { title: 'Express' });
+});
+
+/* POST returns depdoc. */
+router.post('/depdoc', function(req, res, next) {
+	var result = depdoc(req.body, 'json');
+	// console.log(result);
+
+	fs.writeFile('depdoc.md', result, function (err) {
+		if (err) throw err;
+		console.log('It\'s saved!');
+	});
+	// res.setHeader('content-type', 'text/x-markdown');
+	// res.setHeader('content-disposition', 'attachment; filename=depdoc.md');
+
+	res.send(result);
+	res.end();
+});
+
+module.exports = router;
